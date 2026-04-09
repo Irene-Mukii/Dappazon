b8a188f8f880e02fc520c22977ec9d4fc1721c32
Irene Mukii <renniequin@gmail.com>
2026-04-09 18:21:16 +0300

    adds owner adding products to market place feature

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    //code goes here
+   
+    address public owner; // a state variable that stores the address of the owner of the contract.
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 price;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    event List(string name, uint256 price, uint256 quantity); // an event that is emitted when a product is listed on the Dappazon. 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _; //represents the rest of the code in the function that uses this modifier. It is a placeholder for the function body.
+    }
+
+    mapping(uint256 => Item) public items; // a mapping that stores the items (like creating our own a DB table )listed on the Dappazon. The key is the item id and the value is the item struct.
+    constructor() {
+        
+        owner = msg.sender; // msg.sender is a global variable that stores the address of the person who deployed the contract.
+    }
+    //the account owner lists a product in the market place
+    function listProducts(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _price,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner{
+      
+        //creat item struct 
+        Item memory item = Item({
+            id: _id,
+            name: _name,
+            category: _category,
+            image: _image,
+            price: _price,
+            rating: _rating,
+            stock: _stock
+        }); 
+        //save item struct to blockchain
+        items[_id] = item; // save the item struct to the items mapping using the item id as the key.   
+
+        //emit event
+        emit List(_name, _price, _stock); // emit the List event 
+
+    }
+    //buy products
+    //withdraw funds
+
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,63 @@
 const { expect } = require("chai")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.utils.parseUnits(n.toString(), 'ether') //aids in converting to wei
 }
+//global constants for listing an item...
+const ID = 1
+const NAME = "shoes"
+const CATEGORY = "clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const PRICE = tokens(10)
+const RATING = 4
+const STOCK = 5
 
 describe("Dappazon", () => {
+
+  let dappazon;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    //set up accounts
+    [deployer, buyer]= await ethers.getSigners();
+
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+  })
+
+
+  describe("Deployment", () => {
+    it("sets the owner", async () => {
+      const result = await dappazon.owner()
+      expect(result).to.equal(deployer.address)
+    })
+    
+  })
+
+  describe("listing products", () => {
+    let transaction;
+
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).listProducts(ID, NAME, CATEGORY, IMAGE, PRICE, RATING, STOCK)
+      await transaction.wait()
+    })
+
+    it("returns the correct attributes", async () => {
+      const product = await dappazon.items(ID)
+
+      expect(product.id).to.equal(ID)
+      expect(product.name).to.equal(NAME)
+      expect(product.category).to.equal(CATEGORY)
+      expect(product.image).to.equal(IMAGE)
+      expect(product.price).to.equal(PRICE)
+      expect(product.rating).to.equal(RATING)
+      expect(product.stock).to.equal(STOCK)
+    })
+
+    it("emits the List event", async () => {
+      await expect(transaction).to.emit(dappazon, "List").withArgs(NAME, PRICE, STOCK)    
+    })
+  })
 
 })
