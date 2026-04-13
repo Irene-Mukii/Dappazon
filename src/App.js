48565031cc5797847aaf1e15cc99c08149a13d15
Irene Mukii <renniequin@gmail.com>
2026-04-13 17:54:40 +0300

    adds ui navbar that allows connection to a wallet

--- src/App.js
+++ src/App.js
@@ -11,14 +11,59 @@ import Dappazon from './abis/Dappazon.json'
 
 // Config
 import config from './config.json'
+import { use } from 'react'
 
 function App() {
 
+  //useState is a hook that allows you to add state to functional components. It returns an array with two elements: the current state value and a function to update that state. 
+  const [account, setAccount] = useState(null)
+  const [loading, setLoading] = useState(true) 
+
+
+  const loadBlockchainData = async () => {
+  try {
+    // // Force MetaMask if available
+    // const provider = window.ethereum?.providers?.find(p => p.isMetaMask) || window.ethereum
+    // if (!provider) {
+    //   console.error("MetaMask not installed")
+    //   return
+    // }
+      
+    // const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+    // const account = ethers.utils.getAddress(accounts[0])
+    // setAccount(account);
+    // console.log("Account:", account)
+  } 
+  
+  catch (error) {
+    console.error("Error loading blockchain data:", error)
+  }
+  finally {
+    setLoading(false)
+  }
+  }
+
+
+  //useEffect is a hook generally used to perform side effects in functional components,(runs the logic when renders or change in state ie. dependancy arrays occur) such as fetching data, directly updating the DOM, and timers. The empty array [] as the second argument means that this effect will only run once when the component is first mounted, similar to componentDidMount in class components. 
+  useEffect(() => {
+    loadBlockchainData()
+  }, []) 
+
+
+
   return (
     <div>
-
-      <h2>Welcome to Dappazon</h2>
-
+      {loading ? (
+        <div style={{ textAlign: 'center', padding: '50px' }}>
+          <h2>Loading...</h2>
+        </div>
+      ) : (
+        <>
+          <Navigation account={account} setAccount={setAccount} />
+          <h2>Welcome to Dappazon!</h2>
+          <p>Account: {account}</p>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,43 @@ import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
 
+    const connectHandler = async () => {
+        if (account===null) {
+            try {   
+                const provider = window.ethereum?.providers?.find(p => p.isMetaMask) || window.ethereum
+                if (!provider) {
+                    console.error("MetaMask not installed")
+                    return
+                } 
+                const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+                const account = ethers.utils.getAddress(accounts[0])
+                setAccount(account);
+                console.log("Account:", account)
+            } catch (error) {
+                console.error("Error connecting wallet:", error)
+            }  
+        }
+    }
+                   
     return (
         <nav>
+            <div className="nav__brand">
+                <h1>Dappazon</h1>
+            </div>
 
+            <input type="text" placeholder="Search" className="nav__search" />
+            
+            {account ? (
+                <button className="nav__connect">{account.slice(0, 6) + '...'+ account.slice(38, 42)}</button>
+            ) : (
+                <button className="nav__connect" onClick={connectHandler}>Connect Wallet</button>
+            )}
+
+            <ul className="nav__links">
+                <li><a href="#Clothing & Jewelry">Clothing & Jewelry</a></li>
+                <li><a href="#Electronics & Gadgets">Electronics & Gadgets</a></li>
+                <li><a href="#Toys & Gaming">Toys & Gaming</a></li>
+            </ul>
         </nav>
     );
 }
